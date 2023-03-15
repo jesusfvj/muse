f2e3ccfb086b72213025302e1a36b4d273fd82a3
Jesus Velazquez <jesusfvelazquez@hotmail.com>
2023-03-15 10:10:00 +0100

    feat(add tailwind): tailwind install

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ function App() {
 
   return (
     <div className="App">
+      <div className='bg-yellow-300 w-screen h-screen'>
+        <p className='text-center'>Hola inframundo</p>
+      </div>
     </div>
   )
 }
