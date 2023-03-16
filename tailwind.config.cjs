e68195179c694ee4932a3dc668b7fffc76fdccae
Berta Garcia <bertagnavarro93@gmail.com>
2023-03-16 14:49:28 +0100

    Co-authored-by: kriiiiiiiz@icloud.com <kriiiiiiiz@icloud.com>

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,11 @@ module.exports = {
     "./src/**/*.{js,jsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'la-rosalia': "url('src/assets/images/headerPicture.jpeg')",
+      }
+    },
   },
   plugins: [],
 }
