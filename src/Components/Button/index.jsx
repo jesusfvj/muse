c56f98071d82fa15bf75b22c0ac6ec9aec3d2c92
Miquel <abellagarciamiquel@gmail.com>
2023-03-16 11:12:31 +0100

    feat(button and rounded button): added icon prop to button and made rounded button

--- src/Components/Button/index.jsx
+++ src/Components/Button/index.jsx
@@ -7,25 +7,31 @@ export const Button = ({
   outlined = false,
   disabled = false,
   onClick,
+  icon,
 }) => {
   const buttonColors = {
     primary: {
       fill: "bg-gradient-to-tl from-indigo-900 to-blue-500 text-gray-50 enabled:hover:brightness-105",
       outlined:
-        "border border-blue-500 bg-transparent text-white enabled:hover:bg-gradient-to-tl from-indigo-900 to-blue-500",
+        "border border-blue-500 bg-transparent text-gray-300 enabled:hover:bg-gradient-to-tl from-indigo-900 to-blue-500",
     },
     secondary: {
       fill: "bg-gradient-to-tl from-green-900 to-green-500 text-gray-50  enabled:hover:brightness-105",
       outlined:
-        "border border-green-500/90 bg-transparent text-white enabled:hover:bg-gradient-to-tl from-green-900 to-green-500 ",
+        "border border-green-500/90 bg-transparent text-gray-300 enabled:hover:bg-gradient-to-tl from-green-900 to-green-500 ",
     },
     danger: {
       fill: "bg-gradient-to-tl from-red-900 to-red-500 text-gray-50  enabled:hover:brightness-105",
       outlined:
-        "border border-red-500/90 bg-transparent text-white enabled:hover:bg-gradient-to-tl from-red-900 to-red-500",
+        "border border-red-500/90 bg-transparent text-gray-300 enabled:hover:bg-gradient-to-tl from-red-900 to-red-500",
+    },
+    gray: {
+      fill: "bg-gradient-to-tl from-gray-900 to-gray-500 text-gray-300  enabled:hover:brightness-105",
+      outlined:
+        "border border-gray-500/90 bg-transparent text-gray-300 enabled:hover:bg-gradient-to-tl from-gray-900 to-gray-500",
     },
   };
-console.log(outlined)
+
   const sizes = {
     md: "text-sm lg:text-base px-5 py-2",
     sm: "text-xs lg:text-sm px-4 py-1",
@@ -34,10 +40,10 @@ console.log(outlined)
   const type = outlined ? "outlined" : "fill";
   const disabledClass = disabled ? "opacity-50" : "";
 
-  const finalClassName = `centered rounded flex inline-flex justify-center items-center transition ${sizes[size]} ${buttonColors[color][type]} ${disabledClass} hover:${buttonColors[color][type]}/80`;
+  const finalClassName = `w-full lato font-semibold centered rounded flex inline-flex gap-2 justify-center items-center transition ${sizes[size]} ${buttonColors[color][type]} ${disabledClass} hover:${buttonColors[color][type]}/80 active:scale-[0.995] active:translate-y-[0.5px]`;
   return (
     <button disabled={disabled} onClick={onClick} className={finalClassName}>
-      {text}
+      {icon && icon} {text}
     </button>
   );
 };
---
+++ src/Components/RoundButton/index.jsx
@@ -0,0 +1,25 @@
+import { FaPlay } from "react-icons/fa";
+
+export const RoundButton = ({
+  color = "white",
+  background = "white",
+  onClick,
+}) => {
+  const colors = {
+    white: "text-gray-200",
+    gray: "text-gray-600",
+  };
+
+  const backgrounds = {
+    white: "bg-gray-200",
+    gray: "bg-gray-600",
+  };
+
+  const finalClassName = `w-full h-full rounded-full flex items-center justify-center bg-slate-600 ${colors[color]} ${backgrounds[background]}`;
+
+  return (
+    <button className={finalClassName} onClick={onClick && onClick}>
+      <FaPlay />
+    </button>
+  );
+};
