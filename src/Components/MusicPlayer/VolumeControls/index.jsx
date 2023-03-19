069538a5bb9f0cafae0213375ca447387a43e7bb
Miquel <abellagarciamiquel@gmail.com>
2023-03-19 01:00:22 +0100

    styled input type range

--- src/Components/MusicPlayer/VolumeControls/index.jsx
+++ src/Components/MusicPlayer/VolumeControls/index.jsx
@@ -4,7 +4,7 @@ export const VolumeControls = () => {
   return (
     <div className="px-4 py-[1vh] w-full sm:w-auto flex items-center justify-center h-full gap-2">
       <IoMdVolumeHigh className="text-white text-2xl " />
-      <input type="range" className="w-1/2 sm:w-auto"/>
+      <input type="range" className="w-1/2 sm:w-auto muze-range"/>
     </div>
   );
 };
