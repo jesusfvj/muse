bf1a5efec2c924507b01b43ba56422c8744b0c44
Lander Sola <landersola@iMac.local>
2023-03-21 15:43:29 +0100

    small size responsive

--- src/Components/List/ListElement/ArtistElement.jsx
+++ src/Components/List/ListElement/ArtistElement.jsx
@@ -6,8 +6,8 @@ export const ArtistElement = ({ object }) => {
   const { artist, songTitle, album, year, producer, genre, bgImage } = object;
     return (
         <div className="relative flex my-4 min-w-fit">
-            <div className={' bg-slate-900 rounded-[0.5rem] flex flex-col  place-content-between items-center p-2 w-[12rem] h-[15rem] '}>
-                <div className={`w-[8rem] h-[8rem] rounded-full ${bgImage} bg-cover bg-center bg-no-repeat min-h-[8rem] m-4`}>
+            <div className={' bg-slate-900 rounded-[0.5rem] flex flex-col  place-content-between items-center p-2 sm:w-[12rem] sm:h-[15rem] '}>
+                <div className={`sm:w-[8rem] sm:h-[8rem] w-[4rem] h-[4rem] rounded-full ${bgImage} bg-cover bg-center bg-no-repeat sm:min-h-[8rem] m-4`}>
                 </div>
                 <div className="w-full mb-5 px-3 text-center">
                     <Typography text={songTitle} type="p1" color="white" family="lato" styles='max-w-[200px] sm:leading-6 line-clamp-2 text-ellipsis' />
--- src/Components/List/ListElement/SongElement.jsx
+++ src/Components/List/ListElement/SongElement.jsx
@@ -5,10 +5,10 @@ import { Typography, RoundButton } from "../../index";
 export const SongElement = ({ object }) => {
   const { artist, songTitle, album, year, producer, genre, bgImage } = object;
   return (
-    <div className="relative flex my-4 min-w-fit">
+    <div className="relative flex  my-4 min-w-fit">
       <div
         className={
-          " bg-slate-900 rounded-[0.5rem] flex flex-col  place-content-between items-center p-2 w-[12rem] h-[15rem] "
+          " bg-slate-900 rounded-[0.5rem] flex flex-col  place-content-between items-center p-2 sm:w-[12rem] sm:h-[15rem] "
         }
       >
         <div className="w-full mt-2 px-3 ">
@@ -22,7 +22,7 @@ export const SongElement = ({ object }) => {
           <Typography text={artist} type="p2" color="white" family="lato" />
         </div>
         <div
-          className={`w-[8rem] h-[8rem] rounded-full ${bgImage} bg-cover bg-center bg-no-repeat min-h-[8rem] m-4`}
+          className={`w-[4rem] h-[4rem] m-1 sm:w-[8rem] sm:h-[8rem] rounded-full ${bgImage} bg-cover bg-center bg-no-repeat sm:min-h-[8rem] sm:m-4`}
         ></div>
       </div>
       <div className="absolute -bottom-2 -right-2 w-[2.5rem] h-[2.5rem] flex items-center justify-center">
