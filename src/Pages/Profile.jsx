f1f70f9b23f63de9e9f852d20108e7a5b139efaf
Jesus Velazquez <jesusfvelazquez@hotmail.com>
2023-03-21 13:14:03 +0100

    update(header): to ProfileHeader in Profile

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -1,10 +1,10 @@
-import { Body, Header, SecondBody } from "../Components";
+import { Body, ProfileHeader, SecondBody } from "../Components";
 import { Layout } from "../Components/Layout";
 
 export const Profile = () => {
   return (
     <Layout>
-      <Header />
+      <ProfileHeader />
       <div className='w-screen min-h-screen bg-gradient-to-b from-[#4A4A4A] to-[#0A4148]'>
         <Body />
         <SecondBody />
