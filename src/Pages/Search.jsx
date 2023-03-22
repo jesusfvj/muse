386d87b199e0316c885192072b59efba634c9d08
Lander Sola <landersola@iMac.local>
2023-03-22 15:58:34 +0100

    search advance

--- src/Pages/Search.jsx
+++ src/Pages/Search.jsx
@@ -27,10 +27,6 @@ export const Search = () => {
 
   const [searchInput, setSearchInput] = useState("")
   const [searchResults, setsearchResults] = useState({
-    searchResultsSongs: {},
-    searchResultsPlaylists: {},
-    searchResultsAlbums: {},
-    searchResultsArtists: {},
   })
 
 
@@ -41,96 +37,104 @@ export const Search = () => {
     console.log(e.target.value);
   }
   const filter = (searchQuery) => {
-  const searchResultsSongs = {};
-  const searchResultsPlaylists = {};
-  const searchResultsAlbums = {};
-  const searchResultsArtists = {};
+    const searchResultsSongs =[];
+    const searchResultsPlaylists =[];
+    const searchResultsAlbums =[];
+    const searchResultsArtists = [];
 
-  songs.forEach((element) => {
-    if (element.name.toString().toLowerCase().includes(searchQuery.toLowerCase()) || element.artist.toString().toLowerCase().includes(searchQuery.toLowerCase())) {
-      searchResultsSongs[element.id] = element;
-      console.log(element);
-    }
-  });
+    songs.forEach((element) => {
+      if (element.name.toString().toLowerCase().includes(searchQuery.toLowerCase()) || element.artist.toString().toLowerCase().includes(searchQuery.toLowerCase())) {
+        searchResultsSongs[element.id] = element;
+        console.log(element);
+      }
+    });
 
-  playlists.forEach((element) => {
-    if (element.name.toString().toLowerCase().includes(searchQuery.toLowerCase())) {
-      searchResultsPlaylists[element.id] = element;
-      console.log(element);
-    }
-  });
+    playlists.forEach((element) => {
+      if (element.name.toString().toLowerCase().includes(searchQuery.toLowerCase())) {
+        searchResultsPlaylists[element.id] = element;
+        console.log(element);
+      }
+    });
 
-  albums.forEach((element) => {
-    if (element.name.toString().toLowerCase().includes(searchQuery.toLowerCase()) || element.artist.toString().toLowerCase().includes(searchQuery.toLowerCase())) {
-      searchResultsAlbums[element.id] = element;
-      console.log(element);
-    }
-  });
+    albums.forEach((element) => {
+      if (element.name.toString().toLowerCase().includes(searchQuery.toLowerCase()) || element.artist.toString().toLowerCase().includes(searchQuery.toLowerCase())) {
+        searchResultsAlbums[element.id] = element;
+        console.log(element);
+      }
+    });
 
-  artists.forEach((element) => {
-    if (element.name.toString().toLowerCase().includes(searchQuery.toLowerCase())) {
-      searchResultsArtists[element.id] = element;
-      console.log(element);
-    }
-  });
+    artists.forEach((element) => {
+      if (element.name.toString().toLowerCase().includes(searchQuery.toLowerCase())) {
+        searchResultsArtists[element.id] = element;
+        console.log(element);
+      }
+    });
 
-  setsearchResults({
-    searchResultsSongs:{searchResultsSongs},
-    searchResultsPlaylists:{searchResultsPlaylists},
-    searchResultsAlbums:{searchResultsAlbums},
-    searchResultsArtists:{searchResultsArtists},
-  });
-  console.log(searchResults.searchResultsSongs); 
+    setsearchResults(
+     { searchResultsSongs: 
+      searchResultsSongs,
+      searchResultsPlaylists: 
+      searchResultsPlaylists,
+      searchResultsAlbums: 
+      searchResultsAlbums,
+      searchResultsArtists: 
+      searchResultsArtists}
+    
+    );
+  console.log(searchResults.searchResultsSongs);
+  console.log(searchResults.searchResultsPlaylists);
+  console.log(searchResults.searchResultsAlbums);
+  console.log(searchResults.searchResultsArtists);
 
 };
 
-  // const filter = (searchQuery) => {
-  //   console.log(songs);
-  //   const searchResultsSongs = songs.filter((element) => {
-  //     console.log(element.name.toString());
-  //     if (element.name.toString().toLowerCase().includes(searchQuery.toLowerCase()) || element.artist.toString().toLowerCase().includes(searchQuery.toLowerCase())) { return element }
-  //   })
-  //   const searchResultsPlaylists = playlists.filter((element) => {
-  //     if (element.name.toString().toLowerCase().includes(searchQuery.toLowerCase())) { return element }
-  //   })
-  //   const searchResultsAlbums = albums.filter((element) => {
-  //     if (element.name.toString().toLowerCase().includes(searchQuery.toLowerCase()) || element.artist.toString().toLowerCase().includes(searchQuery.toLowerCase())) { return element }
-  //   })
-  //   const searchResultsArtists = artists.filter((element) => {
-  //     if (element.name.toString().toLowerCase().includes(searchQuery.toLowerCase())) { return element }
-  //   })
-  //   setsearchResults({
-  //     searchResultsSongs: {searchResultsSongs},
-  //     searchResultsPlaylists: {searchResultsPlaylists},
-  //     searchResultsAlbums: {searchResultsAlbums},
-  //     searchResultsArtists: {searchResultsArtists},
-  //   })
-  //   console.log();
-  // }
+// const filter = (searchQuery) => {
+//   console.log(songs);
+//   const searchResultsSongs = songs.filter((element) => {
+//     console.log(element.name.toString());
+//     if (element.name.toString().toLowerCase().includes(searchQuery.toLowerCase()) || element.artist.toString().toLowerCase().includes(searchQuery.toLowerCase())) { return element }
+//   })
+//   const searchResultsPlaylists = playlists.filter((element) => {
+//     if (element.name.toString().toLowerCase().includes(searchQuery.toLowerCase())) { return element }
+//   })
+//   const searchResultsAlbums = albums.filter((element) => {
+//     if (element.name.toString().toLowerCase().includes(searchQuery.toLowerCase()) || element.artist.toString().toLowerCase().includes(searchQuery.toLowerCase())) { return element }
+//   })
+//   const searchResultsArtists = artists.filter((element) => {
+//     if (element.name.toString().toLowerCase().includes(searchQuery.toLowerCase())) { return element }
+//   })
+//   setsearchResults({
+//     searchResultsSongs: {searchResultsSongs},
+//     searchResultsPlaylists: {searchResultsPlaylists},
+//     searchResultsAlbums: {searchResultsAlbums},
+//     searchResultsArtists: {searchResultsArtists},
+//   })
+//   console.log();
+// }
 
-  return (
-    <div>
-      <div className='border-black border-2 flex justify-center m-9'>
-        <input type="text" placeholder="Buscar" value={searchInput} className="border-rose-600 border-2 h-12 w-4/12" onChange={(e) => handleChange(e)} />
+return (
+  <div>
+    <div className='border-black border-2 flex justify-center m-9'>
+      <input type="text" placeholder="Buscar" value={searchInput} className="border-rose-600 border-2 h-12 w-4/12" onChange={(e) => handleChange(e)} />
+    </div>
+    <div className='border-black border-2'>
+      <div className="border-rose-600 border-2">
+
+        {searchInput ? <List object={searchResults.searchResultsSongs.filter(Boolean)} sectionTitle="Songs" dataType="song" /> : null}
+
       </div>
-      <div className='border-black border-2'>
-        <div className="border-rose-600 border-2">
+      <div className="border-rose-600 border-2">
+        {/* <List object={searchData.artistas} sectionTitle="Artists" /> */}
 
-         { searchInput ? <List object={searchResults.searchResultsSongs} sectionTitle="Songs" dataType="song" />:null }
+      </div>
+      <div className="border-rose-600 border-2">
+        {/* <List object={searchData.albumes} sectionTitle="Albums" /> */}
 
-        </div>
-        <div className="border-rose-600 border-2">
-          {/* <List object={searchData.artistas} sectionTitle="Artists" /> */}
-
-        </div>
-        <div className="border-rose-600 border-2">
-          {/* <List object={searchData.albumes} sectionTitle="Albums" /> */}
-
-        </div>
-        <div className="border-rose-600 border-2">
-          {/* <List object={searchData.playlists} sectionTitle="Playlists" /> */}
-        </div>
+      </div>
+      <div className="border-rose-600 border-2">
+        {/* <List object={searchData.playlists} sectionTitle="Playlists" /> */}
       </div>
     </div>
-  )
+  </div>
+)
 }
