87deef9c1423ff31db886425a1a7e0baa4145d8f
Miquel <abellagarciamiquel@gmail.com>
2023-03-22 11:48:57 +0100

    removed repeated line

--- src/Components/List/ListElement/SongElement.jsx
+++ src/Components/List/ListElement/SongElement.jsx
@@ -7,7 +7,6 @@ export const SongElement = ({ object }) => {
 
   return (
     <div className="relative flex my-4 select-none">
-    <div className="relative flex my-4 select-none">
       <div
         className={
           " bg-slate-900 rounded-[0.5rem] flex flex-col  place-content-between items-center p-2 w-full h-full "
