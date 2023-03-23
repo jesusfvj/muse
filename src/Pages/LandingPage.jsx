6933859b9199417c072a552ed73185ce9ea17559
Miquel <abellagarciamiquel@gmail.com>
2023-03-23 10:27:28 +0100

    placed logo in landing page and fixed login and register position

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Logo } from "../Components/Logo";
 import { Login, Register } from "../Components/Pages/LandingPage";
 
 export const LandingPage = () => {
@@ -6,6 +7,9 @@ export const LandingPage = () => {
   const changeLogRegister = () => setIsRegistering(!isRegistering);
   return (
     <div className="items-center h-screen bg-[url('./src/assets/images/LandBgImage.png')] bg-cover bg-left-bottom">
+      <div className="absolute top-12">
+        <Logo />
+      </div>
       {isRegistering ? (
         <Register changeLogRegister={changeLogRegister} />
       ) : (
