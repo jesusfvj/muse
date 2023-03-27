499dd72ddc2cadcbe34fa59d10f0b3c90841f5dc
Miquel <abellagarciamiquel@gmail.com>
2023-03-27 13:10:10 +0200

    changed molongui for username in all pages

--- src/Components/Pages/MainPage/Header/index.jsx
+++ src/Components/Pages/MainPage/Header/index.jsx
@@ -1,33 +1,47 @@
-import React from 'react'
-import { Typography } from '../../../Typography'
-
-const arrayTitles = [{
-  text: `Hi, Molongui!`,
-  type: "p1",
-  color: "white",
-  family: "lato",
-  styles: "pb-[2rem]"
-},
-{
-  text: "Listen to your",
-  type: "title",
-  color: "white",
-  family: "lato"
-},
-{
-  text: "favourite music",
-  type: "p1",
-  color: "white",
-  family: "lato",
-  styles: "sm:pl-[6rem] md:pl-[6rem] mt-[-0.3rem]"
-}]
+import React from "react";
+import { useUser } from "../../../../Context/UserContext/UserContext";
+import { Typography } from "../../../Typography";
 
 export const Header = () => {
+  const {
+    user: { fullName },
+  } = useUser();
+  const arrayTitles = [
+    {
+      text: `Hi, ${fullName}!`,
+      type: "p1",
+      color: "white",
+      family: "lato",
+      styles: "pb-[2rem]",
+    },
+    {
+      text: "Listen to your",
+      type: "title",
+      color: "white",
+      family: "lato",
+    },
+    {
+      text: "favourite music",
+      type: "p1",
+      color: "white",
+      family: "lato",
+      styles: "sm:pl-[6rem] md:pl-[6rem] mt-[-0.3rem]",
+    },
+  ];
   return (
     <div className=" h-[20vh]">
-    {arrayTitles.map(({text, type, color, family, styles}, index)=>{
-      return <Typography key={index} text={text} type={type} color={color} family={family} styles={styles}/>
-    })}
+      {arrayTitles.map(({ text, type, color, family, styles }, index) => {
+        return (
+          <Typography
+            key={index}
+            text={text}
+            type={type}
+            color={color}
+            family={family}
+            styles={styles}
+          />
+        );
+      })}
     </div>
-  )
-}
+  );
+};
