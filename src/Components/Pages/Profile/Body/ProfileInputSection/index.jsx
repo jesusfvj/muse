d21502b0c6bbade8c3618a1659b27bb5990b22d1
Jesus Velazquez <jesusfvelazquez@hotmail.com>
2023-03-28 16:31:58 +0200

    update(profile): button and save

--- src/Components/Pages/Profile/Body/ProfileInputSection/index.jsx
+++ src/Components/Pages/Profile/Body/ProfileInputSection/index.jsx
@@ -9,20 +9,26 @@ export const ProfileInputSection = () => {
         event.preventDefault();
     }
     return (
-        <div className='flex flex-col sm:flex-row gap-16 sm:gap-6 lg:gap-16 justify-between items-center'>
-            {arrayInputs.map(({ text, name, type, value, nameTwo, input }, index) => {
-                return <FormWithInput key={`input-${index}`} text={text} name={name} nameTwo={nameTwo} type={type} value={value} input={input} />
-            })}
-            <div className="self-center sm:self-start w-[90%] sm:w-[30%]">
-                <Typography text="Payment details" type="subSection" color="white" family="lato" styles="text-lg mb-[1rem] text-left" />
-                <form className='flex flex-col gap-4 pt-[1rem]'>
-                    <InputElement text="Introduce your bank details" name="Introduce your bank details" type="text" input="text" />
-                    <div className="invisible">
+        <section>
+            <div className='flex flex-col sm:flex-row gap-16 sm:gap-6 lg:gap-16 justify-between items-center'>
+                {arrayInputs.map(({ text, name, type, value, nameTwo, input }, index) => {
+                    return <FormWithInput key={`input-${index}`} text={text} name={name} nameTwo={nameTwo} type={type} value={value} input={input} />
+                })}
+                <div className="self-center sm:self-start w-[90%] sm:w-[30%]">
+                    <Typography text="Payment details" type="subSection" color="white" family="lato" styles="text-lg mb-[1rem] text-left" />
+                    <form className='flex flex-col gap-4 pt-[1rem]'>
                         <InputElement text="Introduce your bank details" name="Introduce your bank details" type="text" input="text" />
-                    </div>
-                    <Button text="Submit" color="black" size="sm" onClick={handleClick} />
-                </form>
+                        <div className="invisible">
+                            <InputElement text="Introduce your bank details" name="Introduce your bank details" type="text" input="text" />
+                        </div>
+                    </form>
+                </div>
             </div>
-        </div>
+            <div className="mt-[4rem] flex justify-start">
+                <div className="w-[10rem]">
+                    <Button text="Save" color="black" size="sm" onClick={handleClick} />
+                </div>
+            </div>
+        </section>
     )
 }
