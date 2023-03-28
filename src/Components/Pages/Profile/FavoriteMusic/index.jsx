642cbde20b9dfeed9ee7b4c2396e8fc6ad411b3d
Miquel <abellagarciamiquel@gmail.com>
2023-03-28 08:28:51 +0200

    implemented loading handling data on fetch

--- src/Components/Pages/Profile/FavoriteMusic/index.jsx
+++ src/Components/Pages/Profile/FavoriteMusic/index.jsx
@@ -12,7 +12,9 @@ import { YourListsSection } from "./YourListsSection";
 import { LovedSection } from "./LovedSection";
 import { useQuery } from "@tanstack/react-query";
 
-export const SecondBody = () => {
+const skeletonData = ["", "", "", "", "", "", "", "", "", "", ""];
+
+export const FavoriteMusic = () => {
   const {
     data: songs,
     isLoading: isLoadingSongs,
@@ -33,20 +35,46 @@ export const SecondBody = () => {
     isLoading: isLoadingPlaylists,
     error: errorPlaylists,
   } = useQuery({ queryKey: ["playlists"], queryFn: getPlaylists });
+
   return (
     <div className="flex flex-col gap-[5rem] min-h-screen bg-gradient-to-b from-[#02040C] to-[#0A4148] xs:ml-[1rem] sm:ml-[3rem] lg:ml-[5rem] pt-[4rem] mt-[8rem] xs:rounded-tl-[3rem] sm:pl-[4rem] sm:pr-[3rem]">
       <div>
         <TitleSection titleSection="Following" />
-        <FollowingSection object={artists} />
+        {!errorArtists ? (
+          <FollowingSection
+            datatype={!isLoadingArtists ? "artist" : "skeletonArtist"}
+            object={!isLoadingArtists ? artists : skeletonData}
+          />
+        ) : (
+          "error"
+        )}
       </div>
+
       <div>
         <TitleSection titleSection="Loved ones" />
-        <LovedSection object1={albums} object2={songs} />
+        {!errorAlbums && !errorSongs ? (
+          <LovedSection
+            datatype1={!isLoadingAlbums ? "album" : "skeletonAlbum"}
+            datatype2={!isLoadingSongs ? "song" : "skeletonSong"}
+            object1={!isLoadingAlbums ? albums : skeletonData}
+            object2={!isLoadingSongs ? songs : skeletonData}
+          />
+        ) : (
+          "error"
+        )}
       </div>
       <div>
         <TitleSection titleSection="Your lists" />
-        <YourListsSection object={playlists} />
+        {!errorPlaylists ? (
+          <FollowingSection
+            datatype={!isLoadingPlaylists ? "playlist" : "skeletonPlaylist"}
+            object={!isLoadingPlaylists ? playlists : skeletonData}
+          />
+        ) : (
+          "error"
+        )}
       </div>
+
       <div>
         <TitleSection titleSection="Most listened songs this week" />
         <AlbumTracks songs={songs2} styles="sm:pr-[3rem]" />
