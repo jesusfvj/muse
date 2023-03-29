bd1dd0ea1031ef0dab7eb7f9c644b64cecc4d1eb
Kristian <kriiiiiiiz@icloud.com>
2023-03-29 13:58:55 +0200

    Deleted unnecessary code

--- src/Components/Dropdown/index.jsx
+++ src/Components/Dropdown/index.jsx
@@ -1,26 +1,57 @@
 import { BsThreeDots } from "react-icons/bs";
 import { Typography } from "../Typography";
 
-export const DropDownMenu = ({ id, activeDropdown, handleToggleDropdown, handleToggleModal }) => {
+export const DropDownMenu = ({
+  id,
+  activeDropdown,
+  handleToggleDropdown,
+  handleToggleModal,
+}) => {
   const isActive = activeDropdown == id;
   return (
     <div className="relative">
-      <button onClick={() => handleToggleDropdown(id)} id="dropdownMenuIconHorizontalButton" className="inline-flex items-center text-sm font-medium text-center text-gray-900 rounded-lg focus:outline-none " type="button"><Typography text={<BsThreeDots />} color="white" /></button>
+      <button
+        onClick={() => handleToggleDropdown(id)}
+        id="dropdownMenuIconHorizontalButton"
+        className="inline-flex items-center text-sm font-medium text-center text-gray-900 rounded-lg focus:outline-none "
+        type="button"
+      >
+        <Typography text={<BsThreeDots />} color="white" />
+      </button>
 
-      <div id={id} className={`z-10 ${ !isActive && "hidden" } absolute right-[0px] divide-y rounded-lg shadow w-44 bg-gray-700 divide-gray-600 `}>
-        <ul className="py-2 text-sm text-white " aria-labelledby="dropdownMenuIconHorizontalButton">
+      <div
+        id={id}
+        className={`z-10 ${
+          !isActive && "hidden"
+        } absolute right-[0px] divide-y rounded-lg shadow w-44 bg-gray-700 divide-gray-600 `}
+      >
+        <ul
+          className="py-2 text-sm text-white "
+          aria-labelledby="dropdownMenuIconHorizontalButton"
+        >
           <li>
-            <a href="#" className="block px-4 py-2  hover:bg-gray-600 "> Play Next </a>
+            <a href="#" className="block px-4 py-2  hover:bg-gray-600 ">
+              Play Next
+            </a>
           </li>
           <li>
-            <a href="#" className="block px-4 py-2  hover:bg-gray-600 "> Go to Artist </a>
+            <a href="#" className="block px-4 py-2  hover:bg-gray-600 ">
+              Go to Artist
+            </a>
           </li>
           <li>
-            <a href="#" className="block px-4 py-2  hover:bg-gray-600 "> Go to Album </a>
+            <a href="#" className="block px-4 py-2  hover:bg-gray-600 ">
+              Go to Album
+            </a>
           </li>
         </ul>
         <div className="py-2">
-          <button onClick={handleToggleModal} className="block px-4 py-2 text-sm text-white hover:bg-gray-600 w-full text-start "> Add to list </button>
+          <button
+            onClick={handleToggleModal}
+            className="block px-4 py-2 text-sm text-white hover:bg-gray-600 w-full text-start "
+          >
+            Add to list
+          </button>
         </div>
       </div>
     </div>
