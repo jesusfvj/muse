0b0694ff5c17e7e6ea524471bbf2f66c86c39951
Miquel <abellagarciamiquel@gmail.com>
2023-04-26 08:20:43 +0200

    added heart to playlist

--- src/Components/PlaylistsHeader/index.jsx
+++ src/Components/PlaylistsHeader/index.jsx
@@ -1,37 +1,52 @@
 import React from "react";
 import { AiFillCaretRight } from "react-icons/ai";
 import { RiShuffleFill } from "react-icons/ri";
+import { AiOutlineHeart, AiFillHeart } from "react-icons/ai";
 import { RoundButton } from "../RoundButton";
 import { Typography } from "../Typography";
 
-export const PlaylistsHeader = ({ name, thumbnail }) => {
+export const PlaylistsHeader = ({ name, thumbnail, isFollowed }) => {
   return (
     <div className="w-screen h-[30vh] sm:h-[50vh] md:h-[60vh] lg:h-[80vh] relative">
       <div className="w-full h-full relative overflow-hidden">
-        <img src={thumbnail} alt={name} className="object-cover w-full h-full blur-md" />
-        <img src={thumbnail} alt={name} className="absolute top-0 bottom-0 left-0 right-0 m-auto z-10 w-2/6" />
+        <img
+          src={thumbnail}
+          alt={name}
+          className="object-cover w-full h-full blur-md"
+        />
+        <img
+          src={thumbnail}
+          alt={name}
+          className="absolute top-0 bottom-0 left-0 right-0 m-auto z-10 w-2/6"
+        />
       </div>
+        <Typography
+          text={!isFollowed ? <AiOutlineHeart /> : <AiFillHeart />}
+          color="white"
+          styles="flex absolute right-10 bottom-0 z-50"
+          type="title"
+        />
       {/* <div className={`bg-cover bg-[url('${thumbnail}')] w-full h-full`}></div> */}
       <div className="bg-gradient-to-b from-[rgba(125,125,125,0)] to-[#02040C] w-screen h-full absolute top-0"></div>
       <div className="flex w-screen h-34 pl-[9.5vw]">
-        <div className="relative h-24 w-29 flex">
-          <div className="w-24">
+        <div className="flex relative">
+          <div className="w-[3rem] h-[3rem] xs:w-[3.6rem] xs:h-[3.6rem] md:w-[4.5rem] md:h-[4.5rem]">
             <RoundButton
               color="white"
               background="white"
-              icon={<AiFillCaretRight size={55} />}
-              margin="pl-1"
+              icon={<AiFillCaretRight size={20} />}
             />
           </div>
-          <div className="h-14 w-14 absolute bottom-[-2vh] right-[-0.7vw]">
+          <div className="w-[1.8rem] h-[1.8rem] xs:w-[2.1rem] xs:h-[2.1rem] md:w-[2.3rem] md:h-[2.3rem] absolute bottom-[0.7vh] right-[-1vw] xs:bottom-[-1vh] xs:right-[-1.2vw] lg:right-[-1vw] xl:right-[-0.5vw]">
             <RoundButton
               color="white"
-              background="dgray"
-              icon={<RiShuffleFill size={22} />}
+              background="darkgray"
+              icon={<RiShuffleFill size={15} />}
+              margin="pt-[0.1rem] xs:pt-0"
             />
           </div>
         </div>
-        <div className="h-16 flex flex-col ml-12">
+        <div className="h-16 w-full flex flex-col ml-12 items-start gap-4">
           <Typography text={name} color="white" type="important" />
         </div>
       </div>
--- src/Pages/Playlist.jsx
+++ src/Pages/Playlist.jsx
@@ -40,7 +40,7 @@ export const Playlist = () => {
     <Layout>
       {songs && (
         <>
-          <PlaylistsHeader name={songs.name} thumbnail={songs.thumbnail} />
+          <PlaylistsHeader name={songs.name} thumbnail={songs.thumbnail} isFollowed={songs.isFollowed}/>
           <div className="flex items-center justify-center bg-gradient-to-b from-[#02040C] to-[#0A4148] w-screen py-48">
             <PlaylistsTracks handleToggleModal={handleToggleModal} songs={songs.tracks} styles="w-full md:w-4/5" />
           </div>
