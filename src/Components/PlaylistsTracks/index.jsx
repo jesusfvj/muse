257a0072fd1ca14a518d87890b841040b222652d
Miquel <abellagarciamiquel@gmail.com>
2023-04-26 15:21:25 +0200

    created add songs modal

--- src/Components/PlaylistsTracks/index.jsx
+++ src/Components/PlaylistsTracks/index.jsx
@@ -3,6 +3,7 @@ import { PlaylistsElements } from "./PlaylistsElements";
 import { AiOutlineHeart, AiFillHeart } from "react-icons/ai";
 import { Typography } from "../Typography";
 import { Button } from "../Button";
+import { AddSongsModal } from "../Pages/Playlist";
 
 export const PlaylistsTracks = ({
   songs,
@@ -11,6 +12,7 @@ export const PlaylistsTracks = ({
   isFollowed,
 }) => {
   const [activeDropdown, setActiveDropdown] = useState(null);
+  const [isAddSongsModalOpen, setIsAddSongsModalOpen] = useState(false);
   const handleToggleDropdown = (id) => {
     if (activeDropdown == id) {
       setActiveDropdown(null);
@@ -22,7 +24,11 @@ export const PlaylistsTracks = ({
     <div className={`flex flex-col ${styles}`}>
       <div className="flex items-center justify-end m-4 gap-4">
         <div>
-          <Button text="Add songs" color="gray"/>
+          <Button
+            text="Add songs"
+            color="gray"
+            onClick={() => setIsAddSongsModalOpen(true)}
+          />
         </div>
         <Typography
           text={!isFollowed ? <AiOutlineHeart /> : <AiFillHeart />}
@@ -46,6 +52,14 @@ export const PlaylistsTracks = ({
           />
         );
       })}
+      {isAddSongsModalOpen && (
+        <div
+          className="fixed top-0 bottom-0 left-0 right-0 m-auto bg-black/90 h-screen w-screen backdrop-blur-sm z-40 flex items-center justify-center"
+          onClick={()=>setIsAddSongsModalOpen(false)}
+        >
+          <AddSongsModal/>
+        </div>
+      )}
     </div>
   );
 };
