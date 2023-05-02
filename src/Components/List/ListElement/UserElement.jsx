7fbf62a835f0c2e8e0a9281a3a639d628e1a9675
Lander Sola <landersola@iMac.local>
2023-05-02 09:26:33 +0200

    Usercomponent

---
+++ src/Components/List/ListElement/UserElement.jsx
@@ -0,0 +1,80 @@
+import { useState } from "react";
+import { FaPlay } from "react-icons/fa";
+import { RiUserFollowFill, RiUserFollowLine } from "react-icons/ri";
+import { Typography, RoundButton } from "../../index";
+import { Dropdown } from "../Dropdown";
+import { Link } from "react-router-dom";
+
+export const UserElement = ({ object }) => {
+  const [clicked, setClicked] = useState(false);
+  const [hovered, setHovered] = useState(false);
+  const [isDropdownActive, setIsDropdownActive] = useState(false);
+
+  const handleOpenDropdown = (e) => {
+     e.stopPropagation()
+    e.preventDefault();
+    setIsDropdownActive(true);
+  };
+
+  const handleMouseLeave = () => {
+    setHovered(false);
+    setIsDropdownActive(false);
+  };
+  const { fullName, profilePic, id } = object;
+  return (
+    // <Link to={`/profile/${id}`}> </Link> 
+    <div
+      className="relative flex my-4 mx-2 select-none shadow-md"
+      onMouseEnter={() => setHovered(true)}
+      onMouseLeave={handleMouseLeave}
+      onContextMenu={handleOpenDropdown}
+    >
+      <div
+        className={
+          " bg-slate-900 rounded-[0.5rem] flex flex-col  place-content-between items-center p-2  w-full h-full "
+        }
+      >
+        <img
+          src={profilePic}
+          className="sm:w-[6rem] sm:h-[6rem] lg:w-[8rem] lg:h-[8rem] w-[4rem] h-[4rem] rounded-full bg-cover bg-center bg-no-repeat lg:min-h-[8rem] m-4 pointer-events-none select-none"
+        />
+        <div className="w-full mb-5 px-3 text-center">
+          <Typography
+            text={fullName}
+            type="p1"
+            color="white"
+            family="lato"
+            styles="sm:leading-6 line-clamp-2 text-ellipsis truncate"
+          />
+        </div>
+      </div>
+      <div
+        className="absolute top-2 right-2 cursor-pointer flex justify-center items-center z-10"
+        onClick={() => (clicked ? setClicked(false) : setClicked(true))}
+      >
+        <Typography
+          text={!clicked ? <RiUserFollowLine /> : <RiUserFollowFill />}
+          type="p0"
+          color={!clicked ? "secondary" : "white"}
+          styles="hidden xs:flex"
+        />
+      </div>
+      {/* <div
+        className={`absolute -bottom-2 -right-2 w-[2.5rem] h-[2.5rem] flex items-center justify-center rounded-full
+      ${hovered ? "flex animation-pop-glow" : "hidden"}`}
+      >
+        <RoundButton
+          color="gray"
+          background="gradient"
+          icon={<FaPlay />}
+          margin="pl-1"
+        />
+      </div> */}
+      <div
+        className={`${!isDropdownActive && "hidden"} absolute right-3 top-12`}
+      >
+        <Dropdown />
+      </div>
+    </div>
+  );
+};
