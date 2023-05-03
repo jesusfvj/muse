455b2f8a3894a5659b1ad65946f5c8c5ea7537a6
Lander Sola <landersolaaguirre@gmail.com>
2023-05-03 12:25:55 +0200

    change on register button

--- src/Components/Pages/LandingPage/Register/index.jsx
+++ src/Components/Pages/LandingPage/Register/index.jsx
@@ -15,17 +15,18 @@ export const Register = ({ changeLogRegister }) => {
 
   const handleRegister = (e) => {
     e.preventDefault();
+    console.log("hola");
     register(registerData);
   };
 
   const handleInputChange = (e) => {
     setRegisterData({ ...registerData, [e.target.name]: e.target.value });
   };
-
+  console.log(registerData);
   return (
     <div className="flex flex-col pt-[25vh] md:pt-32 md:mt-0 md:justify-center h-full w-full 2xl:w-1/4 xl:w-5/12 lg:w-2/5 md:w-2/3 md:ml-24 px-8 gap-8 ">
       <Typography text="Register" color="primary" type="important" />
-      <form className="flex flex-col gap-8"  onSubmit={handleRegister}>
+      <form className="flex flex-col gap-8" >
         <InputWithLabel
           name="fullName"
           label="Enter Your Name"
@@ -54,26 +55,23 @@ export const Register = ({ changeLogRegister }) => {
           value={registerData.repPassword}
           onInputChange={handleInputChange}
         />
-        <div className="w-full flex items-center justify-around">
-          <div className="flex gap-4 items-center">
-            <input
-              type="checkbox"
+        {/* <div className="w-full flex items-center justify-around"> */}
+          {/* <div className="flex gap-4 items-center"> */}
+            <InputWithLabel
               name="role"
-              checked={registerData.isArtist}
-              id="artist"
-              onClick={() => {
+              label="I am an artist"
+              type="checkbox"
+              value={registerData.isArtist}
+              onInputChange={() => {
                 setRegisterData({
                   ...registerData,
                   isArtist: !registerData.isArtist,
                 });
               }}
             />
-            <label htmlFor="artist" className="text-gray-400">
-              I am an artist
-            </label>
-          </div>
-        </div>
-        <Button text="Register" />
+          {/* </div> */}
+        {/* </div> */}
+        <Button text="Register" onClick={handleRegister} />
       </form>
       <div className="flex gap-5">
         <Typography text={"I already have an account"} type="p1" />
