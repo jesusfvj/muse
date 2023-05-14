6f3fd4e63a3743409e2e6b63e564901970aa2b8a
Miquel <abellagarciamiquel@gmail.com>
2023-05-14 18:26:11 +0200

    removed dots from dropdown component

--- src/Components/PlaylistsTracks/PlaylistsElements/index.jsx
+++ src/Components/PlaylistsTracks/PlaylistsElements/index.jsx
@@ -6,6 +6,7 @@ import { DropDownMenu } from "../../Dropdown";
 import { Link } from "react-router-dom";
 import { likeTracks } from "../../../API/MusicApi/MusicApi";
 import { useUser } from "../../../Context/UserContext/UserContext";
+import { BsThreeDots } from "react-icons/bs";
 
 export const PlaylistsElements = ({
   id,
@@ -15,9 +16,11 @@ export const PlaylistsElements = ({
   artist,
   activeDropdown,
   handleToggleDropdown,
-  followedBy
+  followedBy,
 }) => {
-  const { user: { _id: userId } } = useUser()
+  const {
+    user: { _id: userId },
+  } = useUser();
   const [clicked, setClicked] = useState(followedBy.includes(userId));
   const [hovered, setHovered] = useState(false);
   const [buttonDisabled, setButtonDisabled] = useState(false);
@@ -28,7 +31,7 @@ export const PlaylistsElements = ({
 
       setTimeout(() => {
         console.log(!clicked);
-        likeTracks(userId, [id], !clicked)
+        likeTracks(userId, [id], !clicked);
       }, 300);
       setButtonDisabled(true);
       setTimeout(() => {
@@ -36,11 +39,12 @@ export const PlaylistsElements = ({
         setButtonDisabled(false);
       }, 1500);
     }
-  }
+  };
   return (
     <div
-      className={`flex flex-row gap-3 sm:gap-5 items-center justify-between border-b-2 border-white/20 py-5 hover:bg-[#07333f] ${idx === 0 && "border-t-2"
-        }`}
+      className={`flex flex-row gap-3 sm:gap-5 items-center justify-between border-b-2 border-white/20 py-5 hover:bg-[#07333f] ${
+        idx === 0 && "border-t-2"
+      }`}
       onMouseEnter={() => setHovered(true)}
       onMouseLeave={() => {
         setHovered(false);
@@ -49,8 +53,9 @@ export const PlaylistsElements = ({
     >
       <div className="flex items-start justify-start gap-10 md:gap-20 pl-[4vw] md:px-[5vw]">
         <div
-          className={`hidden sm:flex cursor-pointer mt-1 ${hovered ? "visible" : "invisible"
-            }`}
+          className={`hidden sm:flex cursor-pointer mt-1 ${
+            hovered ? "visible" : "invisible"
+          }`}
         >
           <Typography text={<FaPlay />} color="white" />
         </div>
@@ -74,9 +79,18 @@ export const PlaylistsElements = ({
           />
         </div>
         <div
-          className={`cursor-pointer mt-[0.4rem] ${hovered ? "visible" : "sm:invisible"
-            }`}
+          className={`cursor-pointer mt-[0.4rem] ${
+            hovered ? "visible" : "sm:invisible"
+          }`}
         >
+          <button
+            onClick={() => handleToggleDropdown(id)}
+            id="dropdownMenuIconHorizontalButton"
+            className="inline-flex items-center text-sm font-medium text-center text-gray-900 rounded-lg focus:outline-none "
+            type="button"
+          >
+            <Typography text={<BsThreeDots />} color="white" />
+          </button>
           <DropDownMenu
             id={id}
             color="white"
