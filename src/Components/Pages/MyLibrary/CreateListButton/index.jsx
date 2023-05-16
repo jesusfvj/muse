894247c771eeb0ffa00cabdc277a3861930dec58
Miquel <abellagarciamiquel@gmail.com>
2023-05-16 12:34:09 +0200

    updated create playlist style

--- src/Components/Pages/MyLibrary/CreateListButton/index.jsx
+++ src/Components/Pages/MyLibrary/CreateListButton/index.jsx
@@ -8,21 +8,13 @@ export const CreateListButton = () => {
   const [hovered, setHovered] = useState(false);
   return (
     <div
-      className={`relative flex overflow-hidden select-none w-[5rem] h-[7rem] py-4`}
+      className={`relative flex overflow-hidden select-none w-12 h-12 ml-4`}
       onMouseEnter={() => setHovered(true)}
       onMouseLeave={() => setHovered(false)}
       onClick={handleToggleCreatePlaylistModal}
     >
-      <div className=" h-full w-full text-center text-3xl sm:text-6xl flex flex-col justify-center items-center border border-gray-600 hover:border-white  cursor-pointer text-gray-600">
-        {!hovered ? (
-          <AiOutlinePlus />
-        ) : (
-          <Typography
-            color="white"
-            text="Create a new list"
-            styles="z-3 relative"
-          />
-        )}
+      <div className=" h-full w-full text-center text-xl sm:text-2xl flex flex-col justify-center items-center border border-gray-600 hover:border-white  cursor-pointer text-gray-600 rounded-full duration-200">
+        <AiOutlinePlus className={`${hovered && "text-white"}`}/>
       </div>
     </div>
   );
