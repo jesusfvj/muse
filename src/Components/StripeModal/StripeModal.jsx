6ef72a168b2ff2623ba258fc234985bb3920af48
Lander Sola <landersolaaguirre@gmail.com>
2023-05-16 12:07:51 +0200

    Stripe

--- src/Components/StripeModal/StripeModal.jsx
+++ src/Components/StripeModal/StripeModal.jsx
@@ -9,7 +9,7 @@ export const StripeModal = ({ handleToggleStripeModal }) => {
     const [clientSecret, setClientSecret] = useState("");
     const [stripePromise, setStripePromise] = useState("");
 
-    // const stripePromise = loadStripe("pk_test_51N80saK8GYiDB5U2dA7gHIgSiGYxbueVVFSsP5W0c4mTIyr2oPqUiYpeW46ijdYrC4MWUNFIIDKAP06hC9IAsY0400tBSO8Ghc");
+    
     useEffect(() => {
         async function setStripeF() {
             const {publishableKey} = await setStripe()
