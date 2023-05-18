f366c5061a142541300ef2b9bddd86b24c46774e
Miquel <abellagarciamiquel@gmail.com>
2023-05-18 16:29:45 +0200

    added images

--- src/Components/Pages/Profile/ProfileHeader/index.jsx
+++ src/Components/Pages/Profile/ProfileHeader/index.jsx
@@ -7,7 +7,7 @@ export const ProfileHeader = () => {
   } = useUI();
   return (
     <div className="w-screen h-[4rem] xs:h-[8rem] bg-[#96927A] flex flex-col justify-center items-center relative cursor-pointer drop-shadow-xl" onClick={handleToggleStripeModal}>
-      <div className="flex flex-col justify-center items-center h-full w-full bg-[url('https://res.cloudinary.com/dmufnezzd/image/upload/v1684419665/muze-image_file-folder/coffeePattern_rvqqtu.png)] bg-cover">
+      <div className="flex flex-col justify-center items-center h-full w-full bg-[url('https://res.cloudinary.com/dmufnezzd/image/upload/v1684419665/muze-image_file-folder/coffeePattern_rvqqtu.png')] bg-cover">
         <Typography text="Donate" type="important" color="white" family="lato"/>
         <Typography text="buy us a coffee!" type="p1" color="white" family="lato" styles="mt-[-0.75rem] pl-[2rem] xs:pl-[6rem]"/>
       </div>
