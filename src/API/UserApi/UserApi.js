3c7d25d796740b0872e4d4cc3c0648d8c51fda90
Miquel <abellagarciamiquel@gmail.com>
2023-05-19 09:22:49 +0200

    fixed username route

--- src/API/UserApi/UserApi.js
+++ src/API/UserApi/UserApi.js
@@ -1,7 +1,5 @@
 import axios from "axios";
-import {
-  checkTokenExpired
-} from "../../Utils/tokenExpiredValidator";
+import { checkTokenExpired } from "../../Utils/tokenExpiredValidator";
 
 const BASE_URL = "https://muse-back-production.up.railway.app/user";
 
@@ -25,18 +23,22 @@ export const loginUser = async (user) => {
 
 export const followUser = async (loggedUserId, followedUserId, isFollowing) => {
   try {
-    const res = await axios.post(`${BASE_URL}/followUser`, {
-      loggedUserId,
-      followedUserId,
-      isFollowing,
-    }, {
-      headers: {
-        "x-token": window.localStorage.getItem("token")
+    const res = await axios.post(
+      `${BASE_URL}/followUser`,
+      {
+        loggedUserId,
+        followedUserId,
+        isFollowing,
+      },
+      {
+        headers: {
+          "x-token": window.localStorage.getItem("token"),
+        },
       }
-    });
+    );
     return res.data;
   } catch (error) {
-    checkTokenExpired(error.response.data)
+    checkTokenExpired(error.response.data);
     return error.response.data;
   }
 };
@@ -45,51 +47,52 @@ export const getUserById = async (userId) => {
   try {
     const res = await axios.get(`${BASE_URL}/${userId}`, {
       headers: {
-        "x-token": window.localStorage.getItem("token")
-      }
+        "x-token": window.localStorage.getItem("token"),
+      },
     });
     return res.data;
   } catch (error) {
-    checkTokenExpired(error.response.data)
+    checkTokenExpired(error.response.data);
     return error.response.data;
   }
-
 };
 
 export const getArtists = async (id) => {
   try {
     const res = await axios.get(`${BASE_URL}/artists/${id}`, {
       headers: {
-        "x-token": window.localStorage.getItem("token")
-      }
+        "x-token": window.localStorage.getItem("token"),
+      },
     });
     return res.data.artists;
   } catch (error) {
-    checkTokenExpired(error.response.data)
+    checkTokenExpired(error.response.data);
     return error.response.data;
   }
 };
 
 export const changeUsername = async (newUsername, userId) => {
-  const data = await fetch(`${BASE_URL}/user/update-username`, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-        "x-token": window.localStorage.getItem("token")
-      },
-      body: JSON.stringify({
-        username: newUsername,
-        userId: userId,
-      }),
-    })
+  const data = await fetch(`${BASE_URL}/update-username`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      "x-token": window.localStorage.getItem("token"),
+    },
+    body: JSON.stringify({
+      username: newUsername,
+      userId: userId,
+    }),
+  })
     .then((response) => {
       return response.json();
     })
     .then((data) => {
+      console.log("data", data);
       return data;
     })
     .catch((error) => {
-      checkTokenExpired(error.response.data)
+      console.log(error);
+      checkTokenExpired(error.response.data);
     });
   return data;
 };
@@ -98,12 +101,12 @@ export const getFollowedUsers = async (id) => {
   try {
     const res = await axios.get(`${BASE_URL}/followedusers/${id}`, {
       headers: {
-        "x-token": window.localStorage.getItem("token")
-      }
+        "x-token": window.localStorage.getItem("token"),
+      },
     });
     return res.data.users;
   } catch (error) {
-    checkTokenExpired(error.response.data)
+    checkTokenExpired(error.response.data);
     return [];
   }
 };
@@ -112,29 +115,33 @@ export const getArtistById = async (id) => {
   try {
     const res = await axios.get(`${BASE_URL}/artist/${id}`, {
       headers: {
-        "x-token": window.localStorage.getItem("token")
-      }
+        "x-token": window.localStorage.getItem("token"),
+      },
     });
     return res.data.artist;
   } catch (error) {
-    checkTokenExpired(error.response.data)
+    checkTokenExpired(error.response.data);
     return null;
   }
 };
 
 export const handleAddToPlaylist = async (playlistId, trackId) => {
   try {
-    const res = await axios.post(`${BASE_URL}/playlist/addtrack`, {
-      playlistId,
-      trackId,
-    }, {
-      headers: {
-        "x-token": window.localStorage.getItem("token")
+    const res = await axios.post(
+      `${BASE_URL}/playlist/addtrack`,
+      {
+        playlistId,
+        trackId,
+      },
+      {
+        headers: {
+          "x-token": window.localStorage.getItem("token"),
+        },
       }
-    });
+    );
     return res.data;
   } catch (error) {
-    checkTokenExpired(error.response.data)
+    checkTokenExpired(error.response.data);
     return null;
   }
 };
@@ -143,14 +150,16 @@ export const updateProfileImageAPI = async (formData, userId) => {
   try {
     const res = await axios.put(
       `${BASE_URL}/uploadProfileImage/${userId}`,
-      formData, {
+      formData,
+      {
         headers: {
-          "x-token": window.localStorage.getItem("token")
-        }
-      });
+          "x-token": window.localStorage.getItem("token"),
+        },
+      }
+    );
     return res.data;
   } catch (error) {
-    checkTokenExpired(error.response.data)
+    checkTokenExpired(error.response.data);
     return error.response.data;
   }
 };
@@ -161,34 +170,42 @@ export const handleToggleFollowingAlbum = async (
   isFollowed
 ) => {
   try {
-    const res = await axios.put(`${BASE_URL}/toggleFollowPlaylist`, {
-      albumId,
-      userId,
-      isFollowed,
-    }, {
-      headers: {
-        "x-token": window.localStorage.getItem("token")
+    const res = await axios.put(
+      `${BASE_URL}/toggleFollowPlaylist`,
+      {
+        albumId,
+        userId,
+        isFollowed,
+      },
+      {
+        headers: {
+          "x-token": window.localStorage.getItem("token"),
+        },
       }
-    });
+    );
     return res.data.ok;
   } catch (error) {
-    checkTokenExpired(error.response.data)
+    checkTokenExpired(error.response.data);
     return false;
   }
 };
 
 export const sendEmail = async (email) => {
   try {
-    const res = await axios.post(`${BASE_URL}/resetpassword`, {
-      email,
-    }, {
-      headers: {
-        "x-token": window.localStorage.getItem("token")
+    const res = await axios.post(
+      `${BASE_URL}/resetpassword`,
+      {
+        email,
+      },
+      {
+        headers: {
+          "x-token": window.localStorage.getItem("token"),
+        },
       }
-    });
+    );
     return res.data;
   } catch (error) {
-    checkTokenExpired(error.response.data)
+    checkTokenExpired(error.response.data);
     return error.response.data;
   }
 };
@@ -199,36 +216,48 @@ export const changePasswordData = async (
   repeatNewPassword
 ) => {
   try {
-    const res = await axios.post(`${BASE_URL}/resetpasswordchange`, {
-      token,
-      newPassword,
-      repeatNewPassword,
-    }, {
-      headers: {
-        "x-token": window.localStorage.getItem("token")
+    const res = await axios.post(
+      `${BASE_URL}/resetpasswordchange`,
+      {
+        token,
+        newPassword,
+        repeatNewPassword,
+      },
+      {
+        headers: {
+          "x-token": window.localStorage.getItem("token"),
+        },
       }
-    });
+    );
     return res.data;
   } catch (error) {
-    checkTokenExpired(error.response.data)
+    checkTokenExpired(error.response.data);
     return error.response.data;
   }
 };
 
-export const updateUserPassword = async (userId, newPassword, confirmPassword) => {
+export const updateUserPassword = async (
+  userId,
+  newPassword,
+  confirmPassword
+) => {
   try {
-    const res = await axios.post(`${BASE_URL}/resetpasswordprofile`, {
-      userId,
-      newPassword,
-      confirmPassword
-    }, {
-      headers: {
-        "x-token": window.localStorage.getItem("token")
+    const res = await axios.post(
+      `${BASE_URL}/resetpasswordprofile`,
+      {
+        userId,
+        newPassword,
+        confirmPassword,
+      },
+      {
+        headers: {
+          "x-token": window.localStorage.getItem("token"),
+        },
       }
-    });
+    );
     return res.data;
   } catch (error) {
-    checkTokenExpired(error.response.data)
+    checkTokenExpired(error.response.data);
     return null;
   }
 };
